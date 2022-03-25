63c4cec1656957747366cf11c7256f8d15dbe03b
mateus welter <=>
2022-03-25 09:21:30 -0300

    1

--- src/main.js
+++ src/main.js
@@ -5,11 +5,11 @@ import App from './App.vue';
 
 const axios = require('axios');
 
+console.log(process.env.BACKEND_URL);
+
 const http = axios.create({
   baseURL: `${process.env.BACKEND_URL}/todos`,
 });
-
-console.log(http);
 
 Vue.prototype.$http = http;
 
